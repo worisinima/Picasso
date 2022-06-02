76196ac6057f39ec95cad7fe7b00682f34247456
yivanli <yivanli@tencent.com>
2022-06-02 10:37:59 +0800

    SDFTextureGen

--- Picasso/Source/CPURender/CPURender.cpp
+++ Picasso/Source/CPURender/CPURender.cpp
@@ -70,28 +70,79 @@ float distance(const FVectorInt2D& A, const FVectorInt2D& B)
 	return sqrt(Dec.X * Dec.X + Dec.Y * Dec.Y);
 }
 
+int LocToArrayIndex(const FVectorInt2D& PixleLoc, const int& height)
+{
+	int index = PixleLoc.Y * height + PixleLoc.X;
+	if (index > 0 && index < height * height)
+	{
+		return index;
+	}
+	else
+	{
+		LOG("Wrong Pixle Location");
+		return 0;
+	}
+}
+
+FVector2D ConvertPixelLocToUV(const FVectorInt2D& Loc, const int& TextureWidth, const int& TextureHeight)
+{
+	return FVector2D((float)Loc.X / (float)TextureWidth, (float)Loc.Y / (float)TextureHeight);
+}
+
+struct PixleData
+{
+	FColor color;
+	bool bIsInner;
+	FVectorInt2D PixelLoc;
+};
+
+struct FCPUTexture
+{
+	FCPUTexture(){ width = 1; height = 1;}
+	FCPUTexture(const int w, const int h, FColor ClearColor)
+	{
+		width = w;
+		height = h;
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				PixleData NewData;
+				NewData.color = ClearColor;
+				NewData.bIsInner = false;
+				NewData.PixelLoc = FVectorInt2D(x, y);
+				CPUTextureBulkData.push_back(NewData);
+			}
+		}
+	}
+
+	PixleData SampleTexture(FVector2D UV)
+	{
+		FVectorInt2D PixleLoc = FVectorInt2D(UV.X * width, UV.Y * height);
+		return CPUTextureBulkData[PixleLoc.Y * height + PixleLoc.X];
+	}
+
+	int width;
+	int height;
+	vector<PixleData>CPUTextureBulkData;
+};
+
 void CPURenderer::RenderImage()
 {
 	//Begin load texture
 	std::string redTexturePath = gSystemPath + "\\Content\\Textures\\SDFAlphaSource.bmp";
-	struct PixleData
-	{
-		FColor color;
-		bool bIsInner;
-		FVectorInt2D uv;
-	};
-	vector<PixleData>ReadTextureBulkData;
+	
+	FCPUTexture DestTexture(256, 256, FColor(0, 0, 0));
 
-	CImg<uint8_t> SrcTexture(redTexturePath.c_str());
-	int SrcTextureWidth = SrcTexture._width;
-	int SrcTextureHeight = SrcTexture._height;
-	cimg_forXY(SrcTexture, x, y)
+	CImg<uint8_t> LoadSrcTexture(redTexturePath.c_str());
+	FCPUTexture SorcTexture(LoadSrcTexture._width, LoadSrcTexture._height, FColor(0, 0, 0));
+	cimg_forXY(LoadSrcTexture, x, y)
 	{
-		PixleData NewData;
-		NewData.color.R = SrcTexture(x, y, 0);
-		NewData.color.G = SrcTexture(x, y, 1);
-		NewData.color.B = SrcTexture(x, y, 2);
-		NewData.uv = FVectorInt2D(x, y);
+		PixleData& NewData = SorcTexture.CPUTextureBulkData[SorcTexture.width * y + x];
+		NewData.color.R = LoadSrcTexture(x, y, 0);
+		NewData.color.G = LoadSrcTexture(x, y, 1);
+		NewData.color.B = LoadSrcTexture(x, y, 2);
+		NewData.PixelLoc = FVectorInt2D(x, y);
 		if (NewData.color.R > 220 && NewData.color.G > 220 && NewData.color.B > 220)
 		{
 			NewData.bIsInner = true;
@@ -100,33 +151,103 @@ void CPURenderer::RenderImage()
 		{
 			NewData.bIsInner = false;
 		}
-		ReadTextureBulkData.push_back(std::move(NewData));
 	}
 	//End load texture
 
-	float MaxRadius = 30;
-	for (int i = 0; i < ReadTextureBulkData.size(); i++)
+	float MaxRadius = 15;
+	for (int i = 0; i < DestTexture.CPUTextureBulkData.size(); i++)
 	{
-		PixleData& CurColor = ReadTextureBulkData[i];
-		if (CurColor.bIsInner)
+		float MinDistance = MaxRadius * 2;
+
+		PixleData& CurColor = DestTexture.CPUTextureBulkData[i];
+		const FVectorInt2D& CurPixelLoc = CurColor.PixelLoc;
+		FVector2D CurrentPixleUV = ConvertPixelLocToUV(CurPixelLoc, DestTexture.width, DestTexture.height);
+		if (SorcTexture.SampleTexture(CurrentPixleUV).bIsInner == true)
 		{
-			CurColor.color = FColor(255, 0, 0);
+			bool bEnd = false;
+			for (int x = -MaxRadius; x < MaxRadius; x++)
+			{
+				for (int y = -MaxRadius; y < MaxRadius; y++)
+				{
+					FVectorInt2D loc = CurPixelLoc + FVectorInt2D(x, y);
+					loc.X = loc.X >= DestTexture.width ? DestTexture.width - 1 : loc.X;
+					loc.X = loc.X < 0 ? 0 : loc.X;
+					loc.Y = loc.Y >= DestTexture.height ? DestTexture.height - 1 : loc.Y;
+					loc.Y = loc.Y < 0 ? 0 : loc.Y;
+
+					FVector2D UV = ConvertPixelLocToUV(loc, DestTexture.width, DestTexture.height);
+
+					if (SorcTexture.SampleTexture(UV).bIsInner == false)
+					{
+						float NewMinDis = distance(CurPixelLoc, loc);
+						MinDistance = NewMinDis < MinDistance ? NewMinDis : MinDistance;
+						if (MinDistance <= 1)
+						{
+							bEnd = true;
+							break;
+						}
+					}
+				}
+				if (bEnd == true)
+				{
+					break;
+				}
+			}
+
+			float Gradient = (float)MinDistance / (float)MaxRadius;
+			Gradient = Gradient >= 1.0f ? 1.0f : Gradient;
+			CurColor.color = FColor(Gradient * 255, Gradient * 255, Gradient * 255);
 		}
 		else
 		{
-			CurColor.color = FColor(0, 0, 0);
+			bool bEnd = false;
+			for (int x = -MaxRadius; x < MaxRadius; x++)
+			{
+				for (int y = -MaxRadius; y < MaxRadius; y++)
+				{
+					FVectorInt2D loc = CurPixelLoc + FVectorInt2D(x, y);
+					loc.X = loc.X >= DestTexture.width ? DestTexture.width - 1 : loc.X;
+					loc.X = loc.X < 0 ? 0 : loc.X;
+					loc.Y = loc.Y >= DestTexture.height ? DestTexture.height - 1 : loc.Y;
+					loc.Y = loc.Y < 0 ? 0 : loc.Y;
+
+					FVector2D UV = ConvertPixelLocToUV(loc, DestTexture.width, DestTexture.height);
+
+					if (SorcTexture.SampleTexture(UV).bIsInner == true)
+					{
+						float NewMinDis = distance(CurPixelLoc, loc);
+						MinDistance = NewMinDis < MinDistance ? NewMinDis : MinDistance;
+						if (MinDistance <= 1)
+						{
+							bEnd = true;
+							break;
+						}
+					}
+				}
+				if (bEnd == true)
+				{
+					break;
+				}
+			}
+
+			float Gradient = (float)MinDistance / (float)MaxRadius;
+			Gradient = Gradient >= 1.0f ? 1.0f : Gradient;
+			Gradient = 1.0f - Gradient;
+			CurColor.color = FColor(Gradient * 255, Gradient * 255, Gradient * 255);
 		}
+
+		LOG("Finish Image Percentage: " + std::to_string((float)i / (float)DestTexture.CPUTextureBulkData.size() * 100) + "%");
 	}
 
 	//Save the texture
-	CImg<uint8_t> DestTexture(SrcTextureWidth, SrcTextureHeight, 1, 3);
-	cimg_forXY(DestTexture, x, y)
+	CImg<uint8_t> DestSaveTexture(DestTexture.width, DestTexture.height, 1, 3);
+	cimg_forXY(DestSaveTexture, x, y)
 	{
 		FColor NewColor;
-		DestTexture(x, y, 0) = ReadTextureBulkData[y * SrcTextureHeight + x].color.R;
-		DestTexture(x, y, 1) = ReadTextureBulkData[y * SrcTextureHeight + x].color.G;
-		DestTexture(x, y, 2) = ReadTextureBulkData[y * SrcTextureHeight + x].color.B;
+		DestSaveTexture(x, y, 0) = DestTexture.CPUTextureBulkData[y * DestTexture.height + x].color.R;
+		DestSaveTexture(x, y, 1) = DestTexture.CPUTextureBulkData[y * DestTexture.height + x].color.G;
+		DestSaveTexture(x, y, 2) = DestTexture.CPUTextureBulkData[y * DestTexture.height + x].color.B;
 	}
 	const string& Path = FileHelper::GetDesktopPath();
-	DestTexture.save((Path + "\\Text.bmp").c_str());
+	DestSaveTexture.save((Path + "\\Text.bmp").c_str());
 }
